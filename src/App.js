83be7f19920ab8bb465d037d74f47c8df3bb6f11
P S L Yaswanth <yashu.vit@gmail.com>
2020-02-01 23:26:13 +0530

    basic integration micro-frontends

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import MicroFrontend from "./MicroFrontend";
@@ -9,51 +9,30 @@ const {
   REACT_APP_HOST_CART: cartHost
 } = process.env;
 
-console.log("ENV:: productHost --->", process.env.REACT_APP_HOST_CART);
+const Product = ({ history }) => (
+  <MicroFrontend history={history} host={productHost} name="Product" />
+);
+
+const Cart = ({ history }) => (
+  <MicroFrontend history={history} host={cartHost} name="Cart" />
+);
+
+const Shell = ({ history }) => (
+  <div id="appShell">
+    <Product history={history}></Product>
+    <Cart history={history}></Cart>
+  </div>
+);
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
+    <BrowserRouter>
+      <React.Fragment>
         <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
+          <Route exact path="/" component={Shell} />
+          <Route exact path="/product" render={Product} />
         </Switch>
-      </div>
-      <MicroFrontend host={productHost} name="Product"></MicroFrontend>
-    </Router>
+      </React.Fragment>
+    </BrowserRouter>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
--- src/MicroFrontend.js
+++ src/MicroFrontend.js
@@ -1,20 +1,61 @@
 import React from "react";
+
+const injectScript = src => {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = src;
+    script.addEventListener("load", resolve);
+    script.addEventListener("error", () => reject("Error loading script."));
+    script.addEventListener("abort", () => reject("Script loading aborted."));
+    document.head.appendChild(script);
+  });
+};
+
 export default class MicroFrontend extends React.Component {
   componentDidMount() {
+    // only for testing
+
     const { name, host } = this.props;
-    console.log("MicroFrontend Did Mount", name, host);
+    const scriptId = `micro-frontend-script-${name}`;
+    if (document.getElementById(scriptId)) {
+      console.log("Microfrontend: script ID found");
+      // this.renderMicroFrontend();
+      return;
+    }
 
     const hostAssetURL = `${host}/asset-manifest.json`;
     fetch(hostAssetURL)
       .then(res => res.json())
-      .then(result => console.log("Asset manifest", result));
+      .then(manifest => {
+        injectScript(`${host}${manifest["files"]["main.js"]}`)
+          .then(() => {
+            console.log("Script loaded!");
+            this.renderMicroFrontend();
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      });
   }
 
+  renderMicroFrontend = () => {
+    const { name, history } = this.props;
+    console.log(
+      "Microfrontend: RenderMicrofrontend",
+      `render${name}`,
+      `${name}-container`
+    );
+    window[`render${name}`](`${name}-container`, history);
+  };
+
   render() {
-    return <p>Microfrontend!</p>;
+    return <main id={`${this.props.name}-container`} />;
   }
 
   componentWillUnmount() {
-    console.log("Component Unmounted");
+    const { name } = this.props;
+
+    window[`unmount${name}`](`${name}-container`);
   }
 }
